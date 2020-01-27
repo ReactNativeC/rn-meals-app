9e6fa08b80d094802f7034a78746f196a0d19906
Noor <noormahammad@users.noreply.github.com>
2020-01-26 20:49:19 -0500

    Loading Custom Fonts

--- App.js
+++ App.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
+
+const fontFetch = () => {
+  return Font.loadAsync({
+    "OpenSans": require('./fonts/OpenSans-Regular.ttf'),
+    "OpenSans-Bold": require('./fonts/OpenSans-Bold.ttf')
+  });
+};
 
 export default function App() {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  if(!fontsLoaded)
+    return (
+      <AppLoading 
+        startAsync={fontFetch} 
+        onFinish={() => setFontsLoaded(true)} />
+    );
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{fontFamily:'OpenSans'}}>Open up App.js to start working on your app!</Text>
     </View>
   );
 }
