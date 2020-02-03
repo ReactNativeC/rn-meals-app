cee9169796fa626be4bdda7bca1e087bac88477a
Noor <noormahammad@users.noreply.github.com>
2020-02-03 15:22:55 -0500

    Fixing a navigation error in FavoritesScreen

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import MealList from '../components/MealList';
 import { MEALS } from '../data/dummy-data';
-const FavoritesScreen = () => {
+const FavoritesScreen = (props) => {
+  const {navigation} = props;
+    
   const favoriteMeals = MEALS.filter(
     meal => meal.id === "m5" || meal.id === "m3" || meal.id === "m7"
   );
   return (
     <MealList
       data={favoriteMeals}
+      navigation={navigation}
     />
   );
 }
