34143b7d2a413c6eb47ce47b8b478bfc00662d33
Noor <noormahammad@users.noreply.github.com>
2020-02-04 15:54:18 -0500

    fixing Favorites screen - menu was missing

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import MealList from '../components/MealList';
 import { useSelector } from 'react-redux';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+import HeaderButton from '../components/HeaderButton';
 
 const FavoritesScreen = (props) => {
   const {navigation} = props;
@@ -15,8 +17,17 @@ const FavoritesScreen = (props) => {
   );
 }
 
-FavoritesScreen.navigationOptions = {
-  title: 'Your Favorites'
+FavoritesScreen.navigationOptions = navData => { 
+  return {
+    title: 'Your Favorites',
+    headerLeft: () => (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item iconName="ios-menu" title="Menu" onPress={()=>{
+          navData.navigation.toggleDrawer();
+        }} />
+      </HeaderButtons>
+    )
+  }  
 }
 
 export default FavoritesScreen;
