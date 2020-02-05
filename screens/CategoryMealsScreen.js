e00d66dc7737361bd4980e42a7558ffb5e2537d9
Noor <noormahammad@users.noreply.github.com>
2020-02-05 14:57:21 -0500

    when no meals found, display a placeholder message

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import {View, Text, StyleSheet } from 'react-native';
 import { useSelector } from 'react-redux';
 import { CATEGORIES } from '../data/dummy-data';
 import MealList from '../components/MealList';
+import DefaultText from '../components/DefaultText';
+import Colors from '../constants/colors';
 
 const CategoryMealsScreen = (props) => {
   const {navigation} = props;
@@ -12,7 +15,14 @@ const CategoryMealsScreen = (props) => {
   const selectedMeals = availaleMeals.filter(
     meal => meal.categoryIds.indexOf(catId) >= 0
   );
-
+  
+  if(selectedMeals.length === 0)
+    return (
+      <View style={styles.container}>
+        <DefaultText style={styles.errorMessage}>No meals found. Check your filters!</DefaultText>
+      </View>
+    )
+  
   return (    
      <MealList 
       data={selectedMeals}
@@ -28,5 +38,17 @@ CategoryMealsScreen.navigationOptions = ({ navigation }) => {
     title: selectedCategory.title,
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,    
+    justifyContent: 'center',
+    alignItems: 'center'    
+  },  
+  errorMessage: {
+    color: Colors.primaryColor,   
+    fontSize: 20
+  }
+})
 
 export default CategoryMealsScreen;
